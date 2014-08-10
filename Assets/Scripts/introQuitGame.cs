4a5c30827e26f88bce2f88340f529b7dfbc08f7d
cauebs <cauebaasch@gmail.com>
2014-08-09 21:42:44 -0300

    Versão 0.0.2
    Comecei a dar uma organizada. Apaguei arquivos não utilizados, criei
    pastas pra tudo e etc. Sinto muito se você gostava desses arquivos :P
    Amanhã continuo...

--- Assets/Scripts/introQuitGame.cs
+++ Assets/Scripts/introQuitGame.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 using System.Collections;
 
 public class introQuitGame : MonoBehaviour 
-{
-
-	void Start () 
-	{
-	
-	}
-	
+{	
 	void Update () 
 	{
 		if(Input.GetKey(KeyCode.Escape))
